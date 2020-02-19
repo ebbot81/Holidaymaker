b37b034967ac275f52c1c951e089888300676f06
Tobias Severfelt <tobias.severfelt@hotmail.com>
2020-02-19 22:38:41 +0100

    skapat fler metoder och klasser

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -25,7 +25,7 @@ public class Program {
         System.out.println("-----------------------------");
         System.out.println("[1] REGISTRERA NY KUND");
         System.out.println("[2] SÖK LEDIGA RUM");
-        System.out.println("[3] BOKA RUM");
+        System.out.println("[3] HITTA DIN DRÖMRESA");
         System.out.println("[4] AVBOKA RUM");
         System.out.println("[5] AVSLUTA");
 
@@ -46,14 +46,16 @@ public class Program {
                 registerCustomer();
                 break;
             case 2:
+                headLines();
                 ArrayList<Room> vacantRooms = searchVacantRoom();
                 printRooms(vacantRooms, 0);
                 break;
             case 3:
-                bookRoom();
+                bookingMenu();
                 break;
             case 4:
-                //avboka rum
+                ArrayList<Room> occupiedRooms = searchOccupiedRoom();
+                printRooms(occupiedRooms, 0);
                 break;
             case 5:
                 exitProgram();
@@ -64,6 +66,61 @@ public class Program {
         }
     } while (true);
 }
+
+    public void bookingMenu() {
+        int bookMenu;
+        do {
+            System.out.println("\n         HOLIDAYMAKER");
+            System.out.println("-----------------------------");
+            System.out.println("NEDAN FÖLJER OLIKA SÖKKRITERIER FÖR ATT HITTA JUST DIN DRÖMRESA\n");
+            System.out.println("[1] SORTERA EFTER PLATSER MED POOL");
+            System.out.println("[2] SORTERA EFTER PLATSER MED LIVEMUSIK");
+            System.out.println("[3] SORTERA EFTER PLATSER MED BARNKLUBBAR");
+            System.out.println("[4] SORTERA EFTER PLATSER MED RESTAURANGER");
+            System.out.println("[5] BOKA DIN RESA HÄR");
+            System.out.println("[6] AVSLUTA");
+
+            do {
+                try {
+                    bookMenu = Integer.parseInt(scanner.nextLine());
+                    if (bookMenu < 1 || bookMenu > 6) {
+                        throw new IndexOutOfBoundsException();
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Välj ett nummer mellan 1-6");
+                }
+            } while (true);
+
+            switch (bookMenu) {
+                case 1:
+                    headLines();
+                    printRooms(searchRoomWithPool(), 0);
+                    break;
+                case 2:
+                    headLines();
+                    printRooms(searchRoomWithLiveMusic(), 0);
+                    break;
+                case 3:
+                    headLines();
+                    printRooms(searchRoomWithChildrensClub(), 0);
+                    break;
+                case 4:
+                    headLines();
+                    printRooms(searchRoomWithRestaurant(), 0);
+                    break;
+                case 5:
+                    bookRoom();
+
+                    break;
+                case 6:
+                    exitProgram();
+                    break;
+                default:
+                    break;
+            }
+        } while (true);
+    }
 
     private void registerCustomer() {
         System.out.println("Ange kundens namn");
@@ -89,6 +146,18 @@ public class Program {
         }
     }
 
+    public void headLines() {
+        System.out.print(String.format("%3s","ID"));
+        System.out.print(String.format("%16s", "LOCATION"));
+        System.out.print(String.format("%23s", "ROOM_SIZE"));
+        System.out.print(String.format("%13s", "NR_OF_BEDS"));
+        System.out.print(String.format("%7s", "POOL"));
+        System.out.print(String.format("%13s", "LIVE_MUSIC"));
+        System.out.print(String.format("%17s", "CHILDRENS_CLUB"));
+        System.out.print(String.format("%14s", "RESTAURANT\n"));
+        System.out.print(" ********************************************************************************************************\n");
+    }
+
     private void printRooms (ArrayList<Room> rooms, int minNumberOfBeds) {
         for (int i = 0; i < rooms.size(); i++) {
             Room room = rooms.get(i);
@@ -96,12 +165,13 @@ public class Program {
             if(room.getNumberOfBeds() < minNumberOfBeds)
                 continue;
 
-            System.out.printf("id: %d, location: %s, size %s, beds %s \n", room.getId(), room.getLocation(), room.getRoomSize(), room.getNumberOfBeds());
+            System.out.printf(" %-10d%-10s            %-10s      %s         %d         %d               %d              %d \n", room.getId(), room.getLocation(), room.getRoomSize(),
+                    room.getNumberOfBeds(), room.getPool(), room.getLiveMusic(), room.getChildrensClub(), room.getRestaurant());
         }
     }
 
     private ArrayList<Room> searchVacantRoom() {
-        String query = "SELECT r.id_room, r.location, rs.name 'room_size_name', rs.number_of_beds FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id WHERE room_availability = 1";
+        String query = "SELECT r.id_room, r.location, rs.number_of_beds, rs.name 'room_size_name', rs.number_of_beds, pool, live_music, childrens_club, restaurant FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id WHERE room_availability = 1";
         ArrayList<Room> vacantRooms = new ArrayList<>();
         try {
             preparedStatement = connection.prepareStatement(query);
@@ -109,9 +179,13 @@ public class Program {
             while (result.next()) {
                 int id = result.getInt("id_room");
                 int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
                 String location = result.getString("location");
                 String roomSizeName = result.getString("room_size_name");
-                Room room = new Room(id, location, roomSizeName, numberOfBeds);
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
                 vacantRooms.add(room);
             }
             preparedStatement.close();
@@ -119,6 +193,132 @@ public class Program {
             e.printStackTrace();
         }
         return vacantRooms;
+    }
+
+    private ArrayList<Room> searchRoomWithPool() {
+        String query = "SELECT r.id_room, r.location, rs.number_of_beds,  rs.name  'room_size_name', r.pool, r.live_music, r.childrens_club, r.restaurant FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id   WHERE pool = 1";
+        ArrayList<Room> vacantRooms = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet result =  preparedStatement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("id_room");
+                int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
+                String location = result.getString("location");
+                String roomSizeName = result.getString("room_size_name");
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
+                vacantRooms.add(room);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vacantRooms;
+    }
+
+    private ArrayList<Room> searchRoomWithLiveMusic() {
+        String query = "SELECT r.id_room, r.location, rs.number_of_beds,  rs.name  'room_size_name', r.pool, r.live_music, r.childrens_club, r.restaurant FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id   WHERE live_music = 1";
+        ArrayList<Room> vacantRooms = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet result =  preparedStatement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("id_room");
+                int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
+                String location = result.getString("location");
+                String roomSizeName = result.getString("room_size_name");
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
+                vacantRooms.add(room);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vacantRooms;
+    }
+
+    private ArrayList<Room> searchRoomWithChildrensClub() {
+        String query = "SELECT r.id_room, r.location, rs.number_of_beds,  rs.name  'room_size_name', r.pool, r.live_music, r.childrens_club, r.restaurant FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id   WHERE childrens_club = 1";
+        ArrayList<Room> vacantRooms = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet result =  preparedStatement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("id_room");
+                int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
+                String location = result.getString("location");
+                String roomSizeName = result.getString("room_size_name");
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
+                vacantRooms.add(room);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vacantRooms;
+    }
+
+    private ArrayList<Room> searchRoomWithRestaurant() {
+        String query = "SELECT r.id_room, r.location, rs.number_of_beds,  rs.name  'room_size_name', r.pool, r.live_music, r.childrens_club, r.restaurant FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id   WHERE restaurant = 1";
+        ArrayList<Room> vacantRooms = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet result =  preparedStatement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("id_room");
+                int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
+                String location = result.getString("location");
+                String roomSizeName = result.getString("room_size_name");
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
+                vacantRooms.add(room);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vacantRooms;
+    }
+
+
+    private ArrayList<Room> searchOccupiedRoom() {
+        String query = "SELECT r.id_room, r.location, rs.name 'room_size_name', rs.number_of_beds FROM rooms AS r INNER JOIN room_size AS rs ON r.room_size_id = rs.id WHERE room_availability = 0";
+        ArrayList<Room> occupiedRooms = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            ResultSet result =  preparedStatement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("id_room");
+                int numberOfBeds = result.getInt("number_of_beds");
+                int pool = result.getInt("pool");
+                int liveMusic = result.getInt("live_music");
+                int childrensClub = result.getInt("childrens_club");
+                int restaurant = result.getInt("restaurant");
+                String location = result.getString("location");
+                String roomSizeName = result.getString("room_size_name");
+                Room room = new Room(id, location, roomSizeName, numberOfBeds, pool, liveMusic, childrensClub, restaurant);
+                occupiedRooms.add(room);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return occupiedRooms;
     }
 
     private int getCustomerForBooking(){
@@ -163,7 +363,77 @@ public class Program {
         return null;
     }
 
+    private Room getRoomForCancellation(int numberOfBeds){
+
+        ArrayList<Room> vacantRooms = searchVacantRoom();
+
+        printRooms(vacantRooms, numberOfBeds);
+
+        System.out.println("Ange rumsid:et du vill ta bort");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        for(Room room : vacantRooms) {
+            if(room.getId() == id) {
+                vacantRooms.remove(id);
+            }
+        }
+
+        return null;
+    }
+
     private void bookRoom() {
+
+        int customerId = getCustomerForBooking();
+
+        if(customerId < 1){
+            System.out.println("Kan inte hitta kund");
+            return;
+        }
+
+        System.out.println("Ange startdatum");
+        Date startDate = parseDate(scanner.nextLine());
+
+        System.out.println("Ange slutdatum");
+        Date endDate = parseDate(scanner.nextLine());
+        if (endDate.before(startDate)) {
+            System.out.println("Du kan inte resa hem innan du rest iväg :)");
+            return;
+        }
+
+        Date openingDate = parseDate("2020-06-01");
+        Date closedDate = parseDate("2020-07-31");
+
+
+        if (startDate.before(openingDate) || endDate.after(closedDate) ) {
+            System.out.println("Säsongen är inte öppen, välj ett datum mellan 2020-06-01 - 2020-07-31");
+            return;
+        }
+
+        System.out.println("Ange antal resande");
+        int numberOfTravelers = Integer.parseInt(scanner.nextLine());
+        headLines();
+
+        Room room = getRoomForBooking(numberOfTravelers);
+
+
+        String query = "REPLACE INTO bookings ("
+                + "customer_id, start_date, end_date, number_of_customers, room_id)" + "VALUES (?,?,?,?,?)";
+
+        try {
+            preparedStatement = connection.prepareStatement((query));
+            preparedStatement.setString(1, String.valueOf(customerId));
+            preparedStatement.setDate(2, new java.sql.Date(startDate.getTime()), Calendar.getInstance());
+            preparedStatement.setDate(3, new java.sql.Date(endDate.getTime()), Calendar.getInstance());
+            preparedStatement.setString(4, String.valueOf(numberOfTravelers));
+            preparedStatement.setInt(5, room.getId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void cancelRoom() {
 
         int customerId = getCustomerForBooking();
 
@@ -190,9 +460,9 @@ public class Program {
         System.out.println("Ange antal resande");
         int numberOfTravelers = Integer.parseInt(scanner.nextLine());
 
-        Room room = getRoomForBooking(numberOfTravelers);
+        Room room = getRoomForCancellation(numberOfTravelers);
 
-        String query = "REPLACE INTO bookings ("
+        String query = "DELETE FROM bookings ("
                 + "customer_id, start_date, end_date, number_of_customers, room_id)" + "VALUES (?,?,?,?,?)";
 
         try {
@@ -207,10 +477,6 @@ public class Program {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void cancelReservation() {
-
     }
 
     private void connect() {
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -5,13 +5,37 @@ public class Room {
     private int numberOfBeds;
     private String location;
     private String roomSize;
+    private int pool;
+    private int liveMusic;
+    private int childrensClub;
+    private int restaurant;
 
 
-    public Room(int id, String location, String roomSize, int numberOfBeds) {
+    public int getPool() {
+        return pool;
+    }
+
+    public int getLiveMusic() {
+        return liveMusic;
+    }
+
+    public int getChildrensClub() {
+        return childrensClub;
+    }
+
+    public int getRestaurant() {
+        return restaurant;
+    }
+
+    public Room(int id, String location, String roomSize, int numberOfBeds, int pool, int liveMusic, int childrensClub, int restaurant) {
         this.id = id;
         this.location = location;
         this.roomSize = roomSize;
         this.numberOfBeds = numberOfBeds;
+        this.pool = pool;
+        this.liveMusic = liveMusic;
+        this.childrensClub = childrensClub;
+        this.restaurant = restaurant;
     }
 
     public int getId() {
